57ad1fd51112abd9d63666fa97c2d767053bf59f
Bart Trzynadlowski <bart.trzy@gmail.com>
2021-03-03 11:39:31 -0800

    Added method to get all boxes for images and to automatically handle rescaling

--- FasterRCNN/__main__.py
+++ FasterRCNN/__main__.py
@@ -1,3 +1,8 @@
+#
+# Faster R-CNN in Keras: https://towardsdatascience.com/faster-r-cnn-object-detection-implemented-by-keras-for-custom-data-from-googles-open-images-125f62b9141a
+# Understanding RoI pooling: https://towardsdatascience.com/understanding-region-of-interest-part-1-roi-pooling-e4f5dd65bb44
+# NMS for object detection: https://towardsdatascience.com/non-maximum-suppression-nms-93ce178e177c
+
 from . import utils
 from .dataset import VOC
 
@@ -12,3 +17,22 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser("FasterRCNN")
   parser.add_argument("--dataset-dir", metavar = "path", type = str, action = "store", default = "\\projects\\voc\\vocdevkit\\voc2012", help = "Dataset directory")
   options = parser.parse_args()
+
+  from .Models import vgg16
+
+  from tensorflow.keras import Model
+  from tensorflow.keras import Input
+
+
+  conv_model = vgg16.conv_layers(input_shape=(600,800,3))
+
+  model = Model([conv_model.input], conv_model.outputs)
+  model.summary()
+
+  print(conv_model.input)
+
+  print("Loading VOC dataset...")
+  voc = VOC(dataset_dir = options.dataset_dir, scale = 600)
+
+  #print(voc.get_boxes_per_image_path(dataset = "val"))
+  
--- FasterRCNN/dataset.py
+++ FasterRCNN/dataset.py
@@ -1,16 +1,32 @@
 from collections import defaultdict
+import itertools
 import os
 from pathlib import Path
 import xml.etree.ElementTree as ET
 
 class VOC:
-  def __init__(self, dataset_dir):
+  """
+  Loads the VOC dataset at `dataset_dir`. If `scale` is provided, resizes all
+  images and associated metadata (e.g., box coordinates) such that the smallest
+  dimension is equal to `scale`.
+  """
+  def __init__(self, dataset_dir, scale = None):
     self.index_to_class_name = self._get_index_to_class_name(dataset_dir)
     train_image_paths = self._get_image_paths(dataset_dir, dataset = "train")
     val_image_paths = self._get_image_paths(dataset_dir, dataset = "val")
-    descriptions_per_train_image = { image_path: self._get_image_description(dataset_dir, image_path = image_path) for image_path in train_image_paths }
-    descriptions_per_val_image = { image_path: self._get_image_description(dataset_dir, image_path = image_path) for image_path in train_image_paths }
-    print(descriptions_per_val_image)
+    self._descriptions_per_image_path = {}
+    self._descriptions_per_image_path["train"] = { image_path: self._get_image_description(dataset_dir, image_path = image_path, scale = scale) for image_path in train_image_paths }
+    self._descriptions_per_image_path["val"] = { image_path: self._get_image_description(dataset_dir, image_path = image_path, scale = scale) for image_path in train_image_paths }
+
+  def get_boxes_per_image_path(self, dataset):
+    """
+    Returns a dictionary where the key is image path and the value is a list of
+    Box structures.
+    """
+    assert dataset == "train" or dataset == "val"
+    # For each image, get the values from boxes_by_class_name and join them into a single list
+    boxes_per_image_path = { path: list(itertools.chain.from_iterable(image_description.boxes_by_class_name.values())) for path, image_description in self._descriptions_per_image_path[dataset].items() }
+    return boxes_per_image_path
 
   class Box:
     def __init__(self, xmin, ymin, xmax, ymax):
@@ -26,8 +42,10 @@ class VOC:
       return repr(self)
 
   class ImageDescription:
-    def __init__(self,name, width, height, boxes_by_class_name):
+    def __init__(self,name, original_width, original_height, width, height, boxes_by_class_name):
       self.name = name
+      self.original_width = original_width
+      self.original_height = original_height
       self.width = width
       self.height = height
       self.boxes_by_class_name = boxes_by_class_name
@@ -54,7 +72,25 @@ class VOC:
     return image_paths
 
   @staticmethod
-  def _get_image_description(dataset_dir, image_path):
+  def _compute_scale_factor(original_width, original_height, new_scale):
+    if not new_scale:
+      return 1.0
+    return (new_scale / original_height) if original_width > original_height else (new_scale / original_width)
+
+  @staticmethod
+  def _compute_new_scale(original_width, original_height, new_scale):
+    if not new_scale:
+      return (original_width, original_height)
+    if original_width > original_height:
+      new_width = (original_width / original_height) * new_scale
+      new_height = new_scale
+    else:
+      new_height = (original_height / original_width) * new_scale
+      new_width = new_scale
+    return (int(new_width), int(new_height))
+
+  @staticmethod
+  def _get_image_description(dataset_dir, image_path, scale):
     basename = os.path.splitext(os.path.basename(image_path))[0]
     annotation_file = os.path.join(dataset_dir, "Annotations", basename) + ".xml"
     tree = ET.parse(annotation_file)
@@ -65,8 +101,10 @@ class VOC:
     assert len(size.findall("width")) == 1
     assert len(size.findall("height")) == 1
     assert len(size.findall("depth")) == 1
-    width = int(size.find("width").text)
-    height = int(size.find("height").text)
+    original_width = int(size.find("width").text)
+    original_height = int(size.find("height").text)
+    width, height = VOC._compute_new_scale(original_width = original_width, original_height = original_height, new_scale = scale)
+    scale_factor = VOC._compute_scale_factor(original_width = original_width, original_height = original_height, new_scale = scale)
     depth = int(size.find("depth").text)
     assert depth == 3
     boxes_by_class_name = defaultdict(list)
@@ -80,10 +118,15 @@ class VOC:
       assert len(bndbox.findall("ymin")) == 1
       assert len(bndbox.findall("xmax")) == 1
       assert len(bndbox.findall("ymax")) == 1
-      xmin = int(bndbox.find("xmin").text)
-      ymin = int(bndbox.find("ymin").text)
-      xmax = int(bndbox.find("xmax").text)
-      ymax = int(bndbox.find("ymax").text)
+      original_xmin = int(bndbox.find("xmin").text)
+      original_ymin = int(bndbox.find("ymin").text)
+      original_xmax = int(bndbox.find("xmax").text)
+      original_ymax = int(bndbox.find("ymax").text)
+      xmin = original_xmin * scale_factor
+      ymin = original_ymin * scale_factor
+      xmax = original_xmax * scale_factor
+      ymax = original_ymax * scale_factor
+      print("width: %d -> %d\theight: %d -> %d\txmin: %d -> %d\tymin: %d -> %d" % (original_width, width, original_height, height, original_xmin, xmin, original_ymin, ymin))
       box = VOC.Box(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)
       boxes_by_class_name[class_name].append(box)
-    return VOC.ImageDescription(name = basename, width = width, height = height, boxes_by_class_name = boxes_by_class_name)
+    return VOC.ImageDescription(name = basename, original_width = original_width, original_height = original_height, width = width, height = height, boxes_by_class_name = boxes_by_class_name)
