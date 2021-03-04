ff2b04f75e3f9a961c441bf359816864009ce5ef
Bart Trzynadlowski <bart.trzy@gmail.com>
2021-03-03 16:33:45 -0800

    Added function to convert (x,y) coordinate from RPN output map 2D space to input image space

--- FasterRCNN/models/region_proposal_network.py
+++ FasterRCNN/models/region_proposal_network.py
@@ -23,3 +23,17 @@ def layers(input_map, anchors_per_location):
   regressor = Conv2D(name = "rpn_boxes", kernel_size = (1,1), strides = 1, filters = 4 * num_anchors, padding = "same", activation = "linear", kernel_initializer = "zero")(anchors)
 
   return [ classifier, regressor ]
+
+def convert_anchor_coordinate_from_rpn_layer_to_image_space(y, x, image_input_map, anchor_map):
+  """
+  Returns (y, x) converted from the coordinate space of an anchor map (the
+  output of the RPN model) to the original input image space. This gives the
+  anchor center coordinates in the original image.
+  """
+  image_height = image_input_map.shape[1]
+  image_width = image_input_map.shape[2]
+  downstream_height = anchor_map.shape[1]
+  downstream_width = anchor_map.shape[2]
+  y_scale = image_height / downstream_height      # will be pretty close to 16 for VGG
+  x_scale = image_width / downstream_width
+  return (y + 0.5) * y_scale, (x + 0.5) * x_scale # add 0.5 to move into the center of the cell
