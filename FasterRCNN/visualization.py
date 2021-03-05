ef4ad910558f40d91aa2244cd28a6a96b4b5521e
Bart Trzynadlowski <bart.trzy@gmail.com>
2021-03-05 12:14:46 -0800

    Draw anchor points

--- FasterRCNN/visualization.py
+++ FasterRCNN/visualization.py
@@ -5,15 +5,13 @@ from .models import intersection_over_union
 import imageio
 from PIL import Image, ImageDraw
 
-def draw_rectangle(image, x_min, y_min, x_max, y_max, color, thickness = 4):
-  ctx = ImageDraw.Draw(image, mode = "RGBA")
+def draw_rectangle(ctx, x_min, y_min, x_max, y_max, color, thickness = 4):
   ctx.rectangle(xy = [(x_min, y_min), (x_max, y_max)], outline = color, width = thickness)
 
-def draw_filled_rectangle(image, x_min, y_min, x_max, y_max, color):
-  ctx = ImageDraw.Draw(image, mode = "RGBA")
+def draw_filled_rectangle(ctx, x_min, y_min, x_max, y_max, color):
   ctx.rectangle(xy = [(x_min, y_min), (x_max, y_max)], fill = color)
 
-def show_annotated_image(voc, filename, draw_anchor_intersections = False, image_input_map = None, anchor_map = None):
+def show_annotated_image(voc, filename, draw_anchor_points = True, draw_anchor_intersections = False, image_input_map = None, anchor_map = None):
   # Load image
   filepath = voc.get_full_path(filename = filename)
   info = voc.get_image_description(path = filepath)
@@ -27,8 +25,8 @@ def show_annotated_image(voc, filename, draw_anchor_intersections = False, image
   _draw_ground_truth_boxes(image = image, boxes = info.get_boxes())
   
   # Draw anchor boxes and intersection areas, if requested
-  if draw_anchor_intersections:
-    _draw_anchor_box_intersections(image = image, image_input_map = image_input_map, anchor_map = anchor_map, ground_truth_boxes = info.get_boxes())
+  if draw_anchor_intersections or draw_anchor_points:
+    _draw_anchor_box_intersections(image = image, draw_anchor_points = draw_anchor_points, image_input_map = image_input_map, anchor_map = anchor_map, ground_truth_boxes = info.get_boxes())
   
   # Display image
   image.show()
@@ -37,9 +35,11 @@ def _draw_ground_truth_boxes(image, boxes):
   # Draw green boxes
   ctx = ImageDraw.Draw(image)
   for box in boxes:
-    draw_rectangle(image, x_min = box.x_min, y_min = box.y_min, x_max = box.x_max, y_max = box.y_max, color = (0, 255, 0, 255))
+    draw_rectangle(ctx, x_min = box.x_min, y_min = box.y_min, x_max = box.x_max, y_max = box.y_max, color = (0, 255, 0, 255))
+    print("box=%s" % str(box))
 
-def _draw_anchor_box_intersections(image, image_input_map, anchor_map, ground_truth_boxes):
+def _draw_anchor_box_intersections(image, draw_anchor_points, image_input_map, anchor_map, ground_truth_boxes):
+  ctx = ImageDraw.Draw(image, mode = "RGBA")
   anchor_boxes = region_proposal_network.compute_all_anchor_boxes(image_input_map = image_input_map, anchor_map = anchor_map)
   for y in range(anchor_boxes.shape[0]):
     for x in range(anchor_boxes.shape[1]):
@@ -57,17 +57,24 @@ def _draw_anchor_box_intersections(image, image_input_map, anchor_map, ground_tr
         anchor_y_max = center_y + 0.5 * height
         anchor_x_max = center_x + 0.5 * width
 
+        # Draw anchor center pos
+        if draw_anchor_points:
+          size = 1
+          draw_filled_rectangle(ctx, y_min = center_y - size, x_min = center_x - size, y_max = center_y + size, x_max = center_x + size, color = (255, 0, 0, 255)) 
+
         # Does it intersect with any ground truth box?
         for box in ground_truth_boxes:
           box1 = (anchor_y_min, anchor_x_min, anchor_y_max, anchor_x_max)
           box2 = (box.y_min, box.x_min, box.y_max, box.x_max)
 
           iou = intersection_over_union.intersection_over_union(box1 = box1, box2 = box2)
-          if iou > 0:
+          if iou > 0.7:
             # Render intersection area
             intersection_box = intersection_over_union.intersection(box1 = box1, box2 = box2)
-            draw_filled_rectangle(image = image, y_min = intersection_box[0], x_min = intersection_box[1], y_max = intersection_box[2], x_max = intersection_box[3], color = (255, 0, 0, 64))
+            draw_filled_rectangle(ctx, y_min = intersection_box[0], x_min = intersection_box[1], y_max = intersection_box[2], x_max = intersection_box[3], color = (255, 0, 0, 64))
             
             # Draw the anchor box outline
-            draw_rectangle(image, x_min = anchor_x_min, y_min = anchor_y_min, x_max = anchor_x_max, y_max = anchor_y_max, color = (255, 0, 0, 255))
+            draw_rectangle(ctx, x_min = anchor_x_min, y_min = anchor_y_min, x_max = anchor_x_max, y_max = anchor_y_max, color = (255, 0, 0, 255))
 
+          #if center_y < box.y_max and center_y > box.y_min and center_x < box.x_max and center_x > box.x_min:
+          #  print("iou=%f" % iou)
