6142d2ca76ac5ab7dc612c49cf6483bfc2a971a6
Bart Trzynadlowski <trzy@u.washington.edu>
2022-01-18 14:21:45 -0800

    Added download_models.sh

---
+++ download_models.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://trzy.org/files/fasterrcnn/vgg16_caffe.pth
+wget http://trzy.org/files/fasterrcnn/fasterrcnn_pytorch.pth
+wget http://trzy.org/files/fasterrcnn/fasterrcnn_tf2.h5
