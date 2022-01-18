15a2d2c46aa6e9e27bf3eaff5bc66c01c97bfd17
Bart Trzynadlowski <trzy@u.washington.edu>
2022-01-18 13:45:37 -0800

    Training scripts: change model names

--- train_pytorch.sh
+++ train_pytorch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python -m pytorch.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=10 --learning-rate=1e-3 --load-from=vgg16_caffe.pth --save-best-to=pytorch_model_tmp.pth
-python -m pytorch.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=4 --learning-rate=1e-4 --load-from=pytorch_model_tmp.pth --save-best-to=pytorch_model.pth
-rm pytorch_model_tmp.pth
+python -m pytorch.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=10 --learning-rate=1e-3 --load-from=vgg16_caffe.pth --save-best-to=fasterrcnn_pytorch_tmp.pth
+python -m pytorch.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=4 --learning-rate=1e-4 --load-from=fasterrcnn_pytorch.pth --save-best-to=fasterrcnn_pytorch.pth
+rm fasterrcnn_pytorch_tmp.pth
 
--- train_tf2.sh
+++ train_tf2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python -m tf2.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=10 --learning-rate=1e-3 --save-best-to=tf2_model_tmp.pth
-python -m tf2.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=4 --learning-rate=1e-4 --load-from=tf2_model_tmp.pth --save-best-to=tf2_model.pth
-rm tf2_model_tmp.pth
+python -m tf2.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=10 --learning-rate=1e-3 --save-best-to=fasterrcnn_tf2_tmp.h5
+python -m tf2.FasterRCNN --train --dataset-dir=VOCdevkit/VOC2007 --epochs=4 --learning-rate=1e-4 --load-from=fasterrcnn_tf2_tmp.h5 --save-best-to=fasterrcnn_tf2.h5
+rm fasterrcnn_tf2_tmp.h5
