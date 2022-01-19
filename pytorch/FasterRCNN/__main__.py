trzy/FasterRCNN

commit 209141c0e243040259e9ca587a1c2f034441154e

pytorch/FasterRCNN: proposal labeling and sampling is now performed using tensors on GPU rather than escaping to NumPy and CPU

pytorch/FasterRCNN/models/faster_rcnn.py
@@ -40,8 +40,10 @@ class FasterRCNNModel(nn.Module):
     self._num_classes = num_classes
     self._rpn_minibatch_size = rpn_minibatch_size
     self._proposal_batch_size = proposal_batch_size
-    self._detector_box_delta_means = (0, 0, 0, 0)
-    self._detector_box_delta_stds = (0.1, 0.1, 0.2, 0.2)
+    self._detector_box_delta_means = [ 0, 0, 0, 0 ]
+    self._detector_box_delta_stds = [ 0.1, 0.1, 0.2, 0.2 ]
+    self._detector_box_delta_means_tensor = t.tensor(self._detector_box_delta_means, dtype = t.float32).cuda()
+    self._detector_box_delta_stds_tensor = t.tensor(self._detector_box_delta_stds, dtype = t.float32).cuda()
 
     # Network stages
     self._stage1_feature_extractor = vgg16.FeatureExtractor()
@@ -142,6 +144,7 @@ class FasterRCNNModel(nn.Module):
       anchor_map = anchor_map,
       anchor_valid_map = anchor_valid_map
     )
+    proposals = proposals.cpu().numpy() 
     classes = classes.cpu().numpy()
     box_deltas = box_deltas.cpu().numpy()
  
@@ -308,6 +311,13 @@ class FasterRCNNModel(nn.Module):
       positive_fraction = 0.25
     )
 
+    # Make sure RoI proposals and ground truths are detached from computational
+    # graph so that gradients are not propagated through them. They are treated
+    # as constant inputs into the detector stage.
+    proposals = proposals.detach()
+    gt_classes = gt_classes.detach()
+    gt_box_deltas = gt_box_deltas.detach()
+
     # Stage 3: Detector
     detector_classes, detector_box_deltas = self._stage3_detector_network(
       feature_map = feature_map,
@@ -317,8 +327,8 @@ class FasterRCNNModel(nn.Module):
     # Compute losses
     rpn_class_loss = rpn.class_loss(predicted_scores = rpn_score_map, y_true = gt_rpn_minibatch_map)
     rpn_regression_loss = rpn.regression_loss(predicted_box_deltas = rpn_box_deltas_map, y_true = gt_rpn_minibatch_map)
-    detector_class_loss = detector.class_loss(predicted_classes = detector_classes, y_true = t.from_numpy(gt_classes).to("cuda"))
-    detector_regression_loss = detector.regression_loss(predicted_box_deltas = detector_box_deltas, y_true = t.from_numpy(gt_box_deltas).to("cuda"))
+    detector_class_loss = detector.class_loss(predicted_classes = detector_classes, y_true = gt_classes)
+    detector_regression_loss = detector.regression_loss(predicted_box_deltas = detector_box_deltas, y_true = gt_box_deltas)
     loss = FasterRCNNModel.Loss(
       rpn_class = rpn_class_loss,
       rpn_regression = rpn_regression_loss,
@@ -398,7 +408,7 @@ class FasterRCNNModel(nn.Module):
 
     Parameters
     ----------
-    proposals : np.ndarray
+    proposals : torch.Tensor
       Proposal corners, shaped (N, 4).
     gt_boxes : List[datasets.training_sample.Box]
       Ground truth object boxes.
@@ -414,7 +424,7 @@ class FasterRCNNModel(nn.Module):
 
     Returns
     -------
-    np.ndarray, np.ndarray, np.ndarray
+    torch.Tensor, torch.Tensor, torch.Tensor
       Proposals, (N, 4), labeled as either objects or background (depending on
       IoU thresholds, some proposals can end up as neither and are excluded
       here); one-hot encoded class labels, (N, num_classes), for each proposal;
@@ -431,30 +441,30 @@ class FasterRCNNModel(nn.Module):
     assert min_background_iou_threshold < min_object_iou_threshold, "Object threshold must be greater than background threshold"
 
     # Convert ground truth box corners to (M,4) tensor and class indices to (M,)
-    gt_box_corners = np.array([ box.corners for box in gt_boxes ]).astype(np.float32)
-    gt_box_class_idxs = np.array([ box.class_index for box in gt_boxes ]).astype(np.int32)
+    gt_box_corners = t.tensor([ box.corners for box in gt_boxes ], dtype = t.float32).cuda()
+    gt_box_class_idxs = t.tensor([ box.class_index for box in gt_boxes ], dtype = t.long).cuda()
 
     # Let's be crafty and create some fake proposals that match the ground
     # truth boxes exactly. This isn't strictly necessary and the model should
     # work without it but it will help training and will ensure that there are
     # always some positive examples to train on. 
-    proposals = np.vstack([ proposals, gt_box_corners ])
+    proposals = t.vstack([ proposals, gt_box_corners ])
 
     # Compute IoU between each proposal (N,4) and each ground truth box (M,4)
     # -> (N, M)
-    ious = math_utils.intersection_over_union(boxes1 = proposals, boxes2 = gt_box_corners)
+    ious = math_utils.t_intersection_over_union(boxes1 = proposals, boxes2 = gt_box_corners)
 
     # Find the best IoU for each proposal, the class of the ground truth box
     # associated with it, and the box corners
-    best_ious = np.max(ious, axis = 1)              # (N,) of maximum IoUs for each of the N proposals
-    box_idxs = np.argmax(ious, axis = 1)            # (N,) of ground truth box index for each proposal
+    best_ious = t.max(ious, dim = 1).values         # (N,) of maximum IoUs for each of the N proposals
+    box_idxs = t.argmax(ious, dim = 1)              # (N,) of ground truth box index for each proposal
     gt_box_class_idxs = gt_box_class_idxs[box_idxs] # (N,) of class indices of highest-IoU box for each proposal
     gt_box_corners = gt_box_corners[box_idxs]       # (N,4) of box corners of highest-IoU box for each proposal
  
     # Remove all proposals whose best IoU is less than the minimum threshold
     # for a negative (background) sample. We also check for IoUs > 0 because
     # due to earlier clipping, we may get invalid 0-area proposals.
-    idxs = np.where((best_ious >= min_background_iou_threshold))[0]  # keep proposals w/ sufficiently high IoU
+    idxs = t.where((best_ious >= min_background_iou_threshold))[0]  # keep proposals w/ sufficiently high IoU
     proposals = proposals[idxs]
     best_ious = best_ious[idxs]
     gt_box_class_idxs = gt_box_class_idxs[idxs]
@@ -465,8 +475,8 @@ class FasterRCNNModel(nn.Module):
     
     # One-hot encode class labels
     num_proposals = proposals.shape[0]
-    gt_classes = np.zeros((num_proposals, self._num_classes)).astype(np.float32)  # (N,num_classes)
-    gt_classes[ np.arange(num_proposals), gt_box_class_idxs ] = 1.0
+    gt_classes = t.zeros((num_proposals, self._num_classes), dtype = t.float32).cuda()  # (N,num_classes)
+    gt_classes[ t.arange(num_proposals), gt_box_class_idxs ] = 1.0
 
     # Convert proposals and ground truth boxes into "anchor" format (center
     # points and side lengths). For the detector stage, the proposals serve as
@@ -479,19 +489,19 @@ class FasterRCNNModel(nn.Module):
 
     # Compute box delta regression targets (ty, tx, th, tw) for each proposal
     # based on the best box selected
-    box_delta_targets = np.empty((num_proposals, 4))                        # (N,4)
+    box_delta_targets = t.empty((num_proposals, 4), dtype = t.float32).cuda()       # (N,4)
     box_delta_targets[:,0:2] = (gt_box_centers - proposal_centers) / proposal_sides # ty = (gt_center_y - proposal_center_y) / proposal_height, tx = (gt_center_x - proposal_center_x) / proposal_width
-    box_delta_targets[:,2:4] = np.log(gt_box_sides / proposal_sides)                # th = log(gt_height / proposal_height), tw = (gt_width / proposal_width)
-    box_delta_targets[:,:] -= self._detector_box_delta_means                # mean adjustment
-    box_delta_targets[:,:] /= self._detector_box_delta_stds                 # standard deviation scaling
+    box_delta_targets[:,2:4] = t.log(gt_box_sides / proposal_sides)                 # th = log(gt_height / proposal_height), tw = (gt_width / proposal_width)
+    box_delta_targets[:,:] -= self._detector_box_delta_means_tensor                 # mean adjustment
+    box_delta_targets[:,:] /= self._detector_box_delta_stds_tensor                  # standard deviation scaling
 
     # Convert regression targets into a map of shape (N,2,4*(C-1)) where C is
     # the number of classes and [:,0,:] specifies a mask for the corresponding
     # target components at [:,1,:]. Targets are ordered (ty, tx, th, tw).
     # Background class 0 is not present at all.
-    gt_box_deltas = np.zeros((num_proposals, 2, 4 * (self._num_classes - 1))).astype(np.float32)
-    gt_box_deltas[:,0,:] = np.repeat(gt_classes, repeats = 4, axis = 1)[:,4:]         # create masks using interleaved repetition, remembering to ignore class 0
-    gt_box_deltas[:,1,:] = np.tile(box_delta_targets, reps = self._num_classes - 1)   # populate regression targets with straightforward repetition (only those columns corresponding to class are masked on)
+    gt_box_deltas = t.zeros((num_proposals, 2, 4 * (self._num_classes - 1)), dtype = t.float32).cuda()
+    gt_box_deltas[:,0,:] = t.repeat_interleave(gt_classes, repeats = 4, dim = 1)[:,4:]  # create masks using interleaved repetition, remembering to ignore class 0
+    gt_box_deltas[:,1,:] = t.tile(box_delta_targets, dims = (1, self._num_classes - 1)) # populate regression targets with straightforward repetition (only those columns corresponding to class are masked on)
 
     return proposals, gt_classes, gt_box_deltas
 
@@ -500,9 +510,9 @@ class FasterRCNNModel(nn.Module):
       return proposals, gt_classes, gt_box_deltas
   
     # Get positive and negative (background) proposals
-    class_indices = np.argmax(gt_classes, axis = 1)  # (N,num_classes) -> (N,), where each element is the class index (highest score from its row)
-    positive_indices = np.argwhere(class_indices > 0)[:,0]
-    negative_indices = np.argwhere(class_indices <= 0)[:,0]
+    class_indices = t.argmax(gt_classes, axis = 1)  # (N,num_classes) -> (N,), where each element is the class index (highest score from its row)
+    positive_indices = t.where(class_indices > 0)[0]
+    negative_indices = t.where(class_indices <= 0)[0]
     num_positive_proposals = len(positive_indices)
     num_negative_proposals = len(negative_indices)
     
@@ -525,9 +535,9 @@ class FasterRCNNModel(nn.Module):
       return proposals[[]], gt_classes[[]], gt_box_deltas[[]] # return 0-length tensors
   
     # Sample randomly
-    positive_sample_indices = np.random.choice(positive_indices, size = num_positive_samples, replace = False)
-    negative_sample_indices = np.random.choice(negative_indices, size = num_negative_samples, replace = False)
-    indices = np.concatenate([ positive_sample_indices, negative_sample_indices ])
+    positive_sample_indices = positive_indices[ t.randperm(len(positive_indices))[0:num_positive_samples] ]
+    negative_sample_indices = negative_indices[ t.randperm(len(negative_indices))[0:num_negative_samples] ]
+    indices = t.cat([ positive_sample_indices, negative_sample_indices ])
   
     # Return
     return proposals[indices], gt_classes[indices], gt_box_deltas[indices]

tf2/FasterRCNN/__main__.py
@@ -11,8 +11,6 @@
 
 # TODO
 # ----
-# - Speed up PyTorch by converting _label_proposals to use torch rather than NumPy (and use PyTorch equivalent
-#   of stop_gradient where needed). Make sure to perform timing to ensure we don't inadvertently slow things down.
 # - Make a comment in README.md about the importance of tf.stop_gradient (particularly in the loss functions) and
 #   investigate this at some point.
 # - Verify mAP using external program

pytorch/FasterRCNN/models/detector.py
@@ -68,7 +68,6 @@ class DetectorNetwork(nn.Module):
     batch_idxs = t.zeros((proposals.shape[0], 1)).cuda()
 
     # (N, 5) tensor of (batch_idx, x1, y1, x2, y2)
-    proposals = t.from_numpy(proposals).cuda()
     indexed_proposals = t.cat([ batch_idxs, proposals ], dim = 1)
     indexed_proposals = indexed_proposals[:, [ 0, 2, 1, 4, 3 ]] # each row, (batch_idx, y1, x1, y2, x2) -> (batch_idx, x1, y1, x2, y2)
 

pytorch/FasterRCNN/models/math_utils.py
@@ -7,6 +7,7 @@
 #
 
 import numpy as np
+import torch as t
 
 
 def intersection_over_union(boxes1, boxes2):
@@ -32,6 +33,32 @@ def intersection_over_union(boxes1, boxes2):
   areas1 = np.prod(boxes1[:,2:4] - boxes1[:,0:2], axis = 1)                                       # (N,) indicating areas of boxes1
   areas2 = np.prod(boxes2[:,2:4] - boxes2[:,0:2], axis = 1)                                       # (M,) indicating areas of boxes2
   union_areas = areas1[:,None] + areas2 - intersection_areas                                      # (N,1) + (M,) - (N,M) = (N,M), union areas of both boxes
+  epsilon = 1e-7
+  return intersection_areas / (union_areas + epsilon)
+
+def t_intersection_over_union(boxes1, boxes2):
+  """
+  Equivalent to intersection_over_union(), operating on PyTorch tensors.
+
+  Parameters
+  ----------
+  boxes1 : torch.Tensor
+    Box corners, shaped (N, 4), with each box as (y1, x1, y2, x2).
+  boxes2 : torch.Tensor
+    Box corners, shaped (M, 4).
+
+  Returns
+  -------
+  torch.Tensor
+    IoUs for each pair of boxes in boxes1 and boxes2, shaped (N, M).
+  """
+  top_left_point = t.maximum(boxes1[:,None,0:2], boxes2[:,0:2])                                 # (N,1,2) and (M,2) -> (N,M,2) indicating top-left corners of box pairs
+  bottom_right_point = t.minimum(boxes1[:,None,2:4], boxes2[:,2:4])                             # "" bottom-right corners ""
+  well_ordered_mask = t.all(top_left_point < bottom_right_point, axis = 2)                      # (N,M) indicating whether top_left_x < bottom_right_x and top_left_y < bottom_right_y (meaning boxes may intersect)
+  intersection_areas = well_ordered_mask * t.prod(bottom_right_point - top_left_point, dim = 2) # (N,M) indicating intersection area (bottom_right_x - top_left_x) * (bottom_right_y - top_left_y)
+  areas1 = t.prod(boxes1[:,2:4] - boxes1[:,0:2], dim = 1)                                       # (N,) indicating areas of boxes1
+  areas2 = t.prod(boxes2[:,2:4] - boxes2[:,0:2], dim = 1)                                       # (M,) indicating areas of boxes2
+  union_areas = areas1[:,None] + areas2 - intersection_areas                                    # (N,1) + (M,) - (N,M) = (N,M), union areas of both boxes
   epsilon = 1e-7
   return intersection_areas / (union_areas + epsilon)
 
@@ -62,9 +89,40 @@ def convert_deltas_to_boxes(box_deltas, anchors, box_delta_means, box_delta_stds
     Box coordinates, (N, 4), with each row being (y1, x1, y2, x2).
   """
   box_deltas = box_deltas * box_delta_stds + box_delta_means
-  center = anchors[:,2:4] * box_deltas[:,0:2] + anchors[:,0:2] # center_x = anchor_width * tx + anchor_center_x, center_y = anchor_height * ty + anchor_center_y
-  size = anchors[:,2:4] * np.exp(box_deltas[:,2:4])            # width = anchor_width * exp(tw), height = anchor_height * exp(th)
+  center = anchors[:,2:4] * box_deltas[:,0:2] + anchors[:,0:2]  # center_x = anchor_width * tx + anchor_center_x, center_y = anchor_height * ty + anchor_center_y
+  size = anchors[:,2:4] * np.exp(box_deltas[:,2:4])             # width = anchor_width * exp(tw), height = anchor_height * exp(th)
   boxes = np.empty(box_deltas.shape)
+  boxes[:,0:2] = center - 0.5 * size                            # y1, x1
+  boxes[:,2:4] = center + 0.5 * size                            # y2, x2
+  return boxes
+
+def t_convert_deltas_to_boxes(box_deltas, anchors, box_delta_means, box_delta_stds):
+  """
+  Equivalent to convert_deltas_to_boxes(), operating on PyTorch tensors.
+
+  Parameters
+  ----------
+  box_deltas : torch.Tensor
+    Box deltas with shape (N, 4). Each row is (ty, tx, th, tw).
+  anchors : torch.Tensor
+    Corresponding anchors that the box deltas are based upon, shaped (N, 4)
+    with each row being (center_y, center_x, height, width).
+  box_delta_means : torch.Tensor
+    Mean ajustment to box deltas, (4,), to be added after standard deviation
+    scaling and before conversion to actual box coordinates.
+  box_delta_stds : torch.Tensor
+    Standard deviation adjustment to box deltas, (4,). Box deltas are first
+    multiplied by these values.
+
+  Returns
+  -------
+  torch.Tensor
+    Box coordinates, (N, 4), with each row being (y1, x1, y2, x2).
+  """
+  box_deltas = box_deltas * box_delta_stds + box_delta_means
+  center = anchors[:,2:4] * box_deltas[:,0:2] + anchors[:,0:2]  # center_x = anchor_width * tx + anchor_center_x, center_y = anchor_height * ty + anchor_center_y
+  size = anchors[:,2:4] * t.exp(box_deltas[:,2:4])              # width = anchor_width * exp(tw), height = anchor_height * exp(th)
+  boxes = t.empty(box_deltas.shape, dtype = t.float32).cuda()
   boxes[:,0:2] = center - 0.5 * size                            # y1, x1
   boxes[:,2:4] = center + 0.5 * size                            # y2, x2
   return boxes

pytorch/FasterRCNN/models/rpn.py
@@ -69,7 +69,7 @@ class RegionProposalNetwork(nn.Module):
 
     Returns
     -------
-    torch.Tensor, torch.Tensor, np.ndarray
+    torch.Tensor, torch.Tensor, torch.Tensor
       - Objectness scores (batch_size, height, width, num_anchors)
       - Box regressions (batch_size, height, width, num_anchors * 4), as box
         deltas (that is, (ty, tx, th, tw) for each anchor)
@@ -97,41 +97,41 @@ class RegionProposalNetwork(nn.Module):
     )
 
     # Convert regressions to box corners
-    proposals = math_utils.convert_deltas_to_boxes(
+    proposals = math_utils.t_convert_deltas_to_boxes(
       box_deltas = box_deltas,
-      anchors = anchors,
-      box_delta_means = [ 0, 0, 0, 0 ],
-      box_delta_stds = [ 1, 1, 1, 1 ]
-    ).astype(np.float32)
+      anchors = t.from_numpy(anchors).cuda(),
+      box_delta_means = t.tensor([ 0, 0, 0, 0 ], dtype = t.float32).cuda(),
+      box_delta_stds = t.tensor([ 1, 1, 1, 1 ], dtype = t.float32).cuda()
+    ).float()
 
     # Keep only the top-N scores. Note that we do not care whether the
     # proposals were labeled as objects (score > 0.5) and peform a simple
     # ranking among all of them. Restricting them has a strong adverse impact
     # on training performance.
-    sorted_indices = np.argsort(objectness_scores)                  # sort in ascending order of objectness score
-    sorted_indices = sorted_indices[::-1]                           # descending order of score
+    sorted_indices = t.argsort(objectness_scores)                   # sort in ascending order of objectness score
+    sorted_indices = sorted_indices.flip(dims = (0,))               # descending order of score
     proposals = proposals[sorted_indices][0:max_proposals_pre_nms]  # grab the top-N best proposals
     objectness_scores = objectness_scores[sorted_indices][0:max_proposals_pre_nms]  # corresponding scores
 
     # Clip to image boundaries
-    proposals[:,0:2] = np.maximum(proposals[:,0:2], 0)
-    proposals[:,2] = np.minimum(proposals[:,2], image_shape[1])
-    proposals[:,3] = np.minimum(proposals[:,3], image_shape[2])
+    proposals[:,0:2] = t.maximum(proposals[:,0:2], t.tensor([ 0 ], dtype = t.float32).cuda())
+    proposals[:,2] = t.minimum(proposals[:,2], t.tensor([ image_shape[1] ], dtype = t.float32).cuda())
+    proposals[:,3] = t.minimum(proposals[:,3], t.tensor([ image_shape[2] ], dtype = t.float32).cuda())
 
     # Remove anything less than 16 pixels on a side
     height = proposals[:,2] - proposals[:,0]
     width = proposals[:,3] - proposals[:,1]
-    idxs = np.where((height >= 16) & (width >= 16))[0]
+    idxs = t.where((height >= 16) & (width >= 16))[0]
     proposals = proposals[idxs]
     objectness_scores = objectness_scores[idxs]
 
     # Perform NMS
     idxs = nms(
-      boxes = t.from_numpy(proposals).cuda(),
-      scores = t.from_numpy(objectness_scores).cuda(),
+      boxes = proposals, 
+      scores = objectness_scores,
       iou_threshold = 0.7
     )
-    idxs = idxs[0:max_proposals_post_nms].cpu().numpy()
+    idxs = idxs[0:max_proposals_post_nms]
     proposals = proposals[idxs]
 
     # Return network outputs as PyTorch tensors and extracted object proposals
@@ -149,11 +149,13 @@ class RegionProposalNetwork(nn.Module):
 
     if self._allow_edge_proposals:
       # Use all proposals
-      return anchors, scores.cpu().detach().numpy(), box_deltas.cpu().detach().numpy()
+      return anchors, scores, box_deltas
+#      return anchors, scores.cpu().detach().numpy(), box_deltas.cpu().detach().numpy()
     else:
       # Filter out those proposals generated at invalid anchors
       idxs = anchors_valid > 0
-      return anchors[idxs], scores[idxs].cpu().numpy(), box_deltas[idxs].cpu().numpy()
+      return anchors[idxs], scores[idxs], box_deltas[idxs]
+#      return anchors[idxs], scores[idxs].cpu().numpy(), box_deltas[idxs].cpu().numpy()
 
 
 def class_loss(predicted_scores, y_true):
@@ -213,7 +215,6 @@ def regression_loss(predicted_box_deltas, y_true):
   torch.Tensor
     Scalar loss.
   """
-
   epsilon = 1e-7
   scale_factor = 1.0  # hyper-parameter that controls magnitude of regression loss and is chosen to make regression term comparable to class term
   sigma = 3.0         # see: https://github.com/rbgirshick/py-faster-rcnn/issues/89

commit 2a094fc8e027960365a9eb0c5eb50c4e5976ffcc

pytorch/FasterRCNN: CUDA memory leak was introduced in 209141c and this is an unsuccessful attempt to fix that

pytorch/FasterRCNN/__main__.py
@@ -116,16 +116,16 @@ def train(model):
     stats = TrainingStatistics()
     progbar = tqdm(iterable = iter(training_data), total = training_data.num_samples, postfix = stats.get_progbar_postfix())
     for sample in progbar:
-      loss, rpn_score_map, rpn_box_deltas_map, detector_classes, detector_box_deltas, gt_classes, gt_box_deltas = model.train_step(
-          optimizer = optimizer,
-          image_data = t.from_numpy(sample.image_data).unsqueeze(dim = 0).cuda(),
-          anchor_map = sample.anchor_map,
-          anchor_valid_map = sample.anchor_valid_map,
-          gt_rpn_map = t.from_numpy(sample.gt_rpn_map).unsqueeze(dim = 0).cuda(),
-          gt_rpn_object_indices = [ sample.gt_rpn_object_indices ],
-          gt_rpn_background_indices = [ sample.gt_rpn_background_indices ],
-          gt_boxes = [ sample.gt_boxes ]
-        )
+      loss = model.train_step(  # don't retain any tensors we don't need (helps memory usage)
+        optimizer = optimizer,
+        image_data = t.from_numpy(sample.image_data).unsqueeze(dim = 0).cuda(),
+        anchor_map = sample.anchor_map,
+        anchor_valid_map = sample.anchor_valid_map,
+        gt_rpn_map = t.from_numpy(sample.gt_rpn_map).unsqueeze(dim = 0).cuda(),
+        gt_rpn_object_indices = [ sample.gt_rpn_object_indices ],
+        gt_rpn_background_indices = [ sample.gt_rpn_background_indices ],
+        gt_boxes = [ sample.gt_boxes ]
+      )
       stats.on_training_step(loss = loss)
       progbar.set_postfix(stats.get_progbar_postfix())
     last_epoch = epoch == options.epochs

pytorch/FasterRCNN/models/faster_rcnn.py
@@ -27,11 +27,11 @@ from . import detector
 class FasterRCNNModel(nn.Module):
   @dataclass
   class Loss:
-    rpn_class:            t.Tensor
-    rpn_regression:       t.Tensor
-    detector_class:       t.Tensor
-    detector_regression:  t.Tensor
-    total:                t.Tensor
+    rpn_class:            float
+    rpn_regression:       float
+    detector_class:       float
+    detector_regression:  float
+    total:                float
 
   def __init__(self, num_classes, rpn_minibatch_size = 256, proposal_batch_size = 128, allow_edge_proposals = True, dropout_probability = 0):
     super().__init__()
@@ -245,23 +245,9 @@ class FasterRCNNModel(nn.Module):
 
     Returns
     -------
-    Loss, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, np.ndarray,
-    np.ndarray
-   
-      1. Loss (a dataclass with class and regression losses for both the RPN
-         and detector states).
-      2. RPN objectness score map: (batch_size, height, width, num_anchors).
-      3. RPN box deltas map: (batch_size, height, width, num_anchors * 4),
-         where the box deltas are stored in the final dimension in
-         parameterized form ((ty, tx, th, tw) for each anchor).
-      4. Detected classes: (num_proposals, num_classes).
-      5. Detected box deltas: (num_proposals, 4*(num_classes-1)), stored in
-         parameterized form relative to the proposal boxes (which are not
-         returned). Note that class index 0 is the first non-background class.
-      6. Ground truth classes: (num_proposals, num_classes), for the final
-         detection stage.
-      7. Ground truth box deltas: (num_proposals, 2, 4*(num_classes-1)), for
-         the final detection stage.
+    Loss 
+      Loss (a dataclass with class and regression losses for both the RPN and
+      detector states).
     """
     self.train()
 
@@ -329,22 +315,31 @@ class FasterRCNNModel(nn.Module):
     rpn_regression_loss = rpn.regression_loss(predicted_box_deltas = rpn_box_deltas_map, y_true = gt_rpn_minibatch_map)
     detector_class_loss = detector.class_loss(predicted_classes = detector_classes, y_true = gt_classes)
     detector_regression_loss = detector.regression_loss(predicted_box_deltas = detector_box_deltas, y_true = gt_box_deltas)
+    total_loss = rpn_class_loss + rpn_regression_loss + detector_class_loss + detector_regression_loss
     loss = FasterRCNNModel.Loss(
-      rpn_class = rpn_class_loss,
-      rpn_regression = rpn_regression_loss,
-      detector_class = detector_class_loss,
-      detector_regression = detector_regression_loss,
-      total = rpn_class_loss + rpn_regression_loss + detector_class_loss + detector_regression_loss
+      rpn_class = rpn_class_loss.detach().cpu().item(),
+      rpn_regression = rpn_regression_loss.detach().cpu().item(),
+      detector_class = detector_class_loss.detach().cpu().item(),
+      detector_regression = detector_regression_loss.detach().cpu().item(),
+      total = total_loss.detach().cpu().item()
     )
 
     # Backprop
-    loss.total.backward()
+    total_loss.backward()
 
     # Optimizer step
     optimizer.step()
 
+    # Hint to free memory
+    del rpn_score_map
+    del rpn_box_deltas_map
+    del detector_classes
+    del detector_box_deltas
+    del gt_classes
+    del gt_box_deltas
+
     # Return losses and data useful for computing statistics
-    return loss, rpn_score_map, rpn_box_deltas_map, detector_classes, detector_box_deltas, gt_classes, gt_box_deltas
+    return loss
   
   def _sample_rpn_minibatch(self, rpn_map, object_indices, background_indices):
     """

pytorch/FasterRCNN/statistics.py
@@ -36,10 +36,10 @@ class TrainingStatistics:
       Dataclass containing losses broken down by RPN and detector, and further
       by classifier and regression loss. 
     """
-    self._rpn_class_losses.append(loss.rpn_class.detach().cpu().numpy())
-    self._rpn_regression_losses.append(loss.rpn_regression.detach().cpu().numpy())
-    self._detector_class_losses.append(loss.detector_class.detach().cpu().numpy())
-    self._detector_regression_losses.append(loss.detector_regression.detach().cpu().numpy())
+    self._rpn_class_losses.append(loss.rpn_class)
+    self._rpn_regression_losses.append(loss.rpn_regression)
+    self._detector_class_losses.append(loss.detector_class)
+    self._detector_regression_losses.append(loss.detector_regression)
     self.rpn_class_loss = np.mean(self._rpn_class_losses)
     self.rpn_regression_loss = np.mean(self._rpn_regression_losses)
     self.detector_class_loss = np.mean(self._detector_class_losses)