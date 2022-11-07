80cc6116560b68d92da5267045593880856d01fe
Bart Trzynadlowski <bart.trzy@gmail.com>
2022-11-07 00:43:29 -0800

    pytorch/FasterRCNN: Removed manual implementation of ResNet

--- pytorch/FasterRCNN/models/resnet.py
+++ pytorch/FasterRCNN/models/resnet.py
@@ -1,3 +1,8 @@
+#TODO:
+# - Try average pooling again and see if it is any better
+# - Cleanup, particularly of the train() methods and add an explanation for what is going on, tying it to paper if possible
+# - When freezing layers, use layer names rather than array indices
+
 #
 # This repo demonstrates how to use a ResNet backbone. In particular, freezing of the batchnorm layers is very important.
 # TODO: does the ResNet paper, which mentions its deployment in Faster R-CNN, mention this?
@@ -12,10 +17,6 @@
 # The last two dimensions must be eliminated by averaging: .mean(3).mean(2)
 # This produces (N, 2048) which can be fed into detector classifier and regressor layers (inputs to this must be adjusted from 4096->2048)
 
-#
-# TODO: rather than mean, can we just flatten?
-
-# TODO: FeatureExtractor -> Backbone, since it now includes tail
 
 #TODO: dropout only for VGG-16
 
@@ -34,7 +35,6 @@
 # extractor in Faster R-CNN.
 #
 
-import math
 from math import ceil
 import torch as t
 from torch import nn
@@ -42,169 +42,6 @@ from torch.nn import functional as F
 import torchvision
 
 from .feature_extractor import Backbone
-
-
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-  "3x3 convolution with padding"
-  return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-           padding=1, bias=False)
-
-
-class BasicBlock(nn.Module):
-  expansion = 1
-
-  def __init__(self, inplanes, planes, stride=1, downsample=None):
-    super(BasicBlock, self).__init__()
-    self.conv1 = conv3x3(inplanes, planes, stride)
-    self.bn1 = nn.BatchNorm2d(planes)
-    self.relu = nn.ReLU(inplace=True)
-    self.conv2 = conv3x3(planes, planes)
-    self.bn2 = nn.BatchNorm2d(planes)
-    self.downsample = downsample
-    self.stride = stride
-
-  def forward(self, x):
-    residual = x
-
-    out = self.conv1(x)
-    out = self.bn1(out)
-    out = self.relu(out)
-
-    out = self.conv2(out)
-    out = self.bn2(out)
-
-    if self.downsample is not None:
-      residual = self.downsample(x)
-
-    out += residual
-    out = self.relu(out)
-
-    return out
-
-
-class Bottleneck(nn.Module):
-  expansion = 4
-
-  def __init__(self, inplanes, planes, stride=1, downsample=None):
-    super(Bottleneck, self).__init__()
-    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=1, bias=False) # change
-    self.bn1 = nn.BatchNorm2d(planes)
-    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, # change
-                 padding=1, bias=False)
-    self.bn2 = nn.BatchNorm2d(planes)
-    self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-    self.bn3 = nn.BatchNorm2d(planes * 4)
-    self.relu = nn.ReLU(inplace=True)
-    self.downsample = downsample
-    self.stride = stride
-
-  def forward(self, x):
-    residual = x
-
-    out = self.conv1(x)
-    out = self.bn1(out)
-    out = self.relu(out)
-
-    out = self.conv2(out)
-    out = self.bn2(out)
-    out = self.relu(out)
-
-    out = self.conv3(out)
-    out = self.bn3(out)
-
-    if self.downsample is not None:
-      residual = self.downsample(x)
-
-    out += residual
-    out = self.relu(out)
-
-    return out
-
-
-class ResNet(nn.Module):
-  def __init__(self, block, layers, num_classes=1000):
-    self.inplanes = 64
-    super(ResNet, self).__init__()
-    self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                 bias=False)
-    self.bn1 = nn.BatchNorm2d(64)
-    self.relu = nn.ReLU(inplace=True)
-    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=False) # change
-    self.layer1 = self._make_layer(block, 64, layers[0])
-    self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-    self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-    self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-    # it is slightly better whereas slower to set stride = 1
-    # self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
-    self.avgpool = nn.AvgPool2d(7)
-    self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-    for m in self.modules():
-      if isinstance(m, nn.Conv2d):
-        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-      elif isinstance(m, nn.BatchNorm2d):
-        m.weight.data.fill_(1)
-        m.bias.data.zero_()
-
-  def _make_layer(self, block, planes, blocks, stride=1):
-    downsample = None
-    if stride != 1 or self.inplanes != planes * block.expansion:
-      downsample = nn.Sequential(
-        nn.Conv2d(self.inplanes, planes * block.expansion,
-              kernel_size=1, stride=stride, bias=False),
-        nn.BatchNorm2d(planes * block.expansion),
-      )
-
-    layers = []
-    layers.append(block(self.inplanes, planes, stride, downsample))
-    self.inplanes = planes * block.expansion
-    for i in range(1, blocks):
-      layers.append(block(self.inplanes, planes))
-
-    return nn.Sequential(*layers)
-
-  def forward(self, x):
-    x = self.conv1(x)
-    x = self.bn1(x)
-    x = self.relu(x)
-    x = self.maxpool(x)
-
-    x = self.layer1(x)
-    x = self.layer2(x)
-    x = self.layer3(x)
-    x = self.layer4(x)
-
-    x = self.avgpool(x)
-    x = x.view(x.size(0), -1)
-    x = self.fc(x)
-
-    return x
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 class PoolToFeatureVector(nn.Module):
@@ -253,7 +90,6 @@ class FeatureExtractor(nn.Module):
       resnet.layer3
     )
 
-
     # Freeze initial layers
     self._freeze(self._feature_extractor[0])
     self._freeze(self._feature_extractor[1])
@@ -281,8 +117,6 @@ class FeatureExtractor(nn.Module):
           module.eval()
 
       self._feature_extractor.apply(set_bn_eval)
-      #self.RCNN_top.apply(set_bn_eval)
-
 
   def forward(self, image_data):
     y = self._feature_extractor(image_data)
@@ -311,10 +145,6 @@ class ResNetBackbone(Backbone):
     # Construct model and pre-load with ImageNet weights
     resnet = torchvision.models.resnet50(weights = torchvision.models.ResNet50_Weights.IMAGENET1K_V1)
     print("Loaded IMAGENET1K_V1 pre-trained weights for Torchvision ResNet50 feature extractor")
-    state = resnet.state_dict()
-
-    resnet = ResNet(Bottleneck, [ 3, 4, 6, 3])
-    resnet.load_state_dict(state)
 
     # Feature extractor: given image data of shape (batch_size, channels, height, width),
     # produces a feature map of shape (batch_size, 1024, ceil(height/16), ceil(width/16))
